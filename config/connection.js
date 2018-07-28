c139e7ba5e55a75d4646a979527a57e40c39c90d
Carlos Gonzalez <cubeton@gmail.com>
2018-07-28 14:27:29 -0500

    setting conenction

--- config/connection.js
+++ config/connection.js
@@ -4,19 +4,19 @@ var connection;
 if (process.env.JAWSDB_URL) {
   connection = mysql.createConnection(process.env.JAWSDB_URL);
 } else {
-  connection = mysql.createConnection({
-    host: "localhost",
-    user: "root",
-    password: "root",
-    database: "points"
-  });
-
   // connection = mysql.createConnection({
-  //   host: 'localhost',
-  //   user: 'kh25u3iu1dz254qk',
-  //   password: 'a26s5k6hjtq66m4p',
-  //   database: 'r44ezfddc6fcbn66'
+  //   host: "localhost",
+  //   user: "root",
+  //   password: "root",
+  //   database: "points"
   // });
+
+  connection = mysql.createConnection({
+    host: "g8mh6ge01lu2z3n1.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
+    user: "t7t1nd1jpz9eapq0",
+    password: "qq2rmsamhtll8wq6",
+    database: "up7k0kxbuvdxw75n"
+  });
 }
 
 module.exports = connection;
