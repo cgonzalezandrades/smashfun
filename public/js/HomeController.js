5d9989a6e5f9e978b015045a822839ecd0fe99d2
Carlos Gonzalez <cubeton@gmail.com>
2018-07-28 22:42:26 -0500

    WIP

--- public/js/HomeController.js
+++ public/js/HomeController.js
@@ -6,8 +6,25 @@ myApp.controller("HomeController", [
   "$http",
   function($rootScope, $scope, $state, $stateParams, $http) {
     //    console.log('ins home controller')
-    $scope.newUser = {};
-    $scope.userIn = true;
+    $scope.newUser = {
+      name: "",
+      lastName: "",
+      pictureLink: "",
+      alias: ""
+    };
+    $scope.startFight = false;
+
+    $scope.deletePlayer = function(userId) {
+      $http.post("/deleteUser", { userId: userId }).then(
+        function success(response) {
+          console.log(response.data);
+          $scope.getUsers();
+        },
+        function errorCallback(error) {
+          console.log(error);
+        }
+      );
+    };
 
     $scope.getUsers = function() {
       $http.get("/users").then(function success(response) {
@@ -25,34 +42,31 @@ myApp.controller("HomeController", [
     };
     $scope.getScores();
 
+    $scope.getData = function() {
+      $http.get("/data").then(function success(response) {
+        console.log(response.data);
+        $scope.scores = response.data;
+      });
+    };
+    $scope.getData();
+
     //  $scope.users = {something:'ss', another:'sds'}
 
     $scope.userConnected = function(index) {
       $scope.users[index].ADDED = true;
     };
 
-    $scope.formFilled = function() {
-      if (
-        $scope.newUser.ACCOUNT_FIRST_NAME.length > 0 &&
-        $scope.newUser.ACCOUNT_LAST_NAME.length > 0 &&
-        $scope.newUser.ACCOUNT_LINK.length > 0
-      ) {
-        $scope.userIn = false;
-      } else {
-        $scope.userIn = true;
-      }
-    };
-
-    $scope.addNewUser = function() {
-      $http.post("/addAccount", $scope.newUser).then(
-        function success(response) {},
+    $scope.saveUser = function() {
+      $http.post("/addUser", $scope.newUser).then(
+        function success(response) {
+          console.log("success");
+          console.log(JSON.parse(response.data));
+          $("#addUserModal").modal("toggle");
+        },
         function errorCallback(error) {
           console.log(error);
         }
       );
-
-      $scope.getAccounts();
-      $scope.newUser = {};
     };
   }
 ]);
--- server.js
+++ server.js
@@ -66,6 +66,69 @@ app.get("/scores", function(req, res) {
   });
 });
 
+app.get("/data", function(req, res) {
+  //  console.log(req.body)
+  // connection.query('INSERT INTO LINKEDIN_ACCOUNTS (ACCOUNT_FIRST_NAME, ACCOUNT_LAST_NAME, ACCOUNT_LINK) VALUES(?,?,?)', [req.body.ACCOUNT_FIRST_NAME, req.body.ACCOUNT_LAST_NAME, req.body.ACCOUNT_LINK], function (error, results, field) {
+  //   if (error) throw error;
+  // })
+
+  connection.query(
+    "SELECT * FROM points.SCORES AS A JOIN points.USERS AS B ON A.USER_ID = B.USER_ID",
+    function(error, results, fields) {
+      if (error) throw error;
+      // connected!
+
+      var formattedData = JSON.stringify(results);
+
+      //  console.log(results[0].ACCOUNT_FIRST_NAME)
+      //  console.log(results)
+      res.json(results);
+    }
+  );
+});
+
+app.post("/addUser", function(req, res) {
+  console.log(req.body);
+  connection.query(
+    "INSERT INTO USERS (FIRST_NAME, LAST_NAME, PATH, NICKNAME) VALUES(?,?,?,?)",
+    [req.body.name, req.body.lastName, req.body.pictureLink, req.body.alias],
+    function(error, results) {
+      if (error) throw error;
+
+      var formattedData = JSON.stringify(results);
+      insertIntoScore(results.insertId);
+      res.json(formattedData);
+    }
+  );
+});
+
+app.post("/deleteUser", function(req, res) {
+  console.log(req.body);
+  connection.query(
+    "DELETE FROM USERS WHERE USER_ID = " + req.body.userId,
+    // [req.body.userId],
+    function(error, results) {
+      if (error) throw error;
+
+      var formattedData = JSON.stringify(results);
+      insertIntoScore(results.insertId);
+      res.json(formattedData);
+    }
+  );
+});
+
+function insertIntoScore(userId) {
+  console.log(userId);
+  connection.query(
+    "INSERT INTO SCORES (FIRST_PLACE_SCORE, SECOND_PLACE_SCORE, THIRD_PLACE_SCORE, DAMAGE_SCORE, KILL_SCORE, TOTAL, USER_ID) VALUES(?,?,?,?,?,?,?)",
+    [0, 0, 0, 0, 0, 0, userId],
+    function(error, results) {
+      if (error) throw error;
+      console.log(results);
+    }
+  );
+}
+
 var PORT = process.env.PORT || 3001;
 app.listen(PORT, function() {
   //  console.log('IM LISTENING IS PORT ' + PORT);
