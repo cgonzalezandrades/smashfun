382eb50d29e7fc831f99a52e2b3c8c84c819379c
Carlos Gonzalez <cubeton@gmail.com>
2018-07-28 23:17:22 -0500

    WIP

--- public/config/connection.js
+++ public/config/connection.js
@@ -12,7 +12,7 @@ if (process.env.JAWSDB_URL) {
   // });
 
   connection = mysql.createConnection({
-    host: "localhost",
+    host: "g8mh6ge01lu2z3n1.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
     user: "t7t1nd1jpz9eapq0",
     password: "qq2rmsamhtll8wq6",
     database: "up7k0kxbuvdxw75n"
