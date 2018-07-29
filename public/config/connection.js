800c9378ce3510318f3dcf062841888eb23a8d59
Carlos Gonzalez <cubeton@gmail.com>
2018-07-28 23:09:50 -0500

    WIP

--- public/config/connection.js
+++ public/config/connection.js
@@ -4,19 +4,19 @@ var connection;
 if (process.env.JAWSDB_URL) {
   connection = mysql.createConnection(process.env.JAWSDB_URL);
 } else {
+  // connection = mysql.createConnection({
+  //   host: "localhost",
+  //   user: "root",
+  //   password: "root",
+  //   database: "points"
+  // });
+
   connection = mysql.createConnection({
     host: "localhost",
-    user: "root",
-    password: "root",
-    database: "points"
+    user: "kh25u3iu1dz254qk",
+    password: "a26s5k6hjtq66m4p",
+    database: "r44ezfddc6fcbn66"
   });
-
-  // connection = mysql.createConnection({
-  //   host: 'localhost',
-  //   user: 'kh25u3iu1dz254qk',
-  //   password: 'a26s5k6hjtq66m4p',
-  //   database: 'r44ezfddc6fcbn66'
-  // });
 }
 
 module.exports = connection;
