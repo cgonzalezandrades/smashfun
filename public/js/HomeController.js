c825c84db602ae6d199e5856289c724052395791
Carlos Gonzalez <cubeton@gmail.com>
2018-08-18 17:11:21 -0500

    WIP

--- public/js/HomeController.js
+++ public/js/HomeController.js
@@ -6,12 +6,15 @@ myApp.controller("HomeController", [
   "$http",
   function($rootScope, $scope, $state, $stateParams, $http) {
     //    console.log('ins home controller')
+    $scope.fighterModal = { IMAGE: "", NAME: "" };
+    $scope.fights = [];
     $scope.newUser = {
       name: "",
       lastName: "",
       pictureLink: "",
       alias: ""
     };
+
     $scope.positions = ["1re Lugar", "2do Lugar", "3re Lugar"];
     $scope.startFight = false;
     $scope.modes = [{ TYPE: "1 Vs 1" }, { TYPE: "Todos Contra Todos" }];
@@ -19,6 +22,28 @@ myApp.controller("HomeController", [
     $scope.optionSelected = function(optionSelected) {
       $scope.modeSelected = optionSelected;
       console.log(optionSelected);
+    };
+    $scope.resetObject = function() {
+      $scope.fighterModal = { IMAGE: "", NAME: "" };
+    };
+    $scope.beginFight = function() {
+      console.log($scope.fighterModal);
+
+      $scope.fights.push({
+        NAME: $scope.fighterModal.NAME,
+        IMAGE: $scope.fighterModal.IMAGE
+      });
+
+      console.log($scope.fights);
+    };
+
+    $scope.fighterSelected = function(fighterSelected) {
+      $scope.fighterModal.IMAGE = fighterSelected.PICTURE_PATH;
+      $scope.fighterModal.NAME = fighterSelected.NAME;
+    };
+
+    $scope.positionSelected = function(positionSelected) {
+      $scope.position = positionSelected;
     };
 
     $scope.deletePlayer = function(userId) {
@@ -52,6 +77,7 @@ myApp.controller("HomeController", [
     $scope.getFighters = function() {
       $http.get("/figthers").then(function success(response) {
         console.log(response.data);
+
         $scope.fighters = response.data;
       });
     };
