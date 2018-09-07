d01ea0fa1154966529822500c05590e9e29733b0
Carlos Gonzalez <cubeton@gmail.com>
2018-09-06 22:59:22 -0500

    change damage points

--- public/js/HomeController.js
+++ public/js/HomeController.js
@@ -36,6 +36,71 @@ myApp.controller("HomeController", [
     const POINTS_PER_DAMAGE = 0.01;
     const POINTS_PER_KILLS = 1;
 
+    // Set the date we're counting down to
+    var countDownDate = new Date("Dec 7, 2018 24:00:00").getTime();
+    var countDownTorunament = new Date("Dec 6, 2018 24:00:00").getTime();
+
+    // Update the count down every 1 second
+    var x = setInterval(function() {
+      // Get todays date and time
+      var now = new Date().getTime();
+
+      // Find the distance between now and the count down date
+      var distance = countDownDate - now;
+
+      // Time calculations for days, hours, minutes and seconds
+      var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+      var hours = Math.floor(
+        (distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
+      );
+      var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+      var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+      // Output the result in an element with id="demo"
+      document.getElementById("demo").innerHTML =
+        days +
+        " dias " +
+        hours +
+        " horas " +
+        minutes +
+        " minutos " +
+        seconds +
+        " segundos ";
+
+      // If the count down is over, write some text
+      if (distance < 0) {
+        clearInterval(x);
+        document.getElementById("demo").innerHTML = "EXPIRED";
+      }
+    }, 1000);
+
+    var y = setInterval(function() {
+      // Get todays date and time
+      var now = new Date().getTime();
+
+      // Find the distance between now and the count down date
+      var distance = countDownTorunament - now;
+
+      // Time calculations for days, hours, minutes and seconds
+      var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+      var hours = Math.floor(
+        (distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
+      );
+      var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+      var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+      // Output the result in an element with id="demo"
+      document.getElementById("countdown-tournament").innerHTML =
+        days + " dias " + hours + " horas " + minutes + " minutos ";
+
+      // If the count down is over, write some text
+      if (distance < 0) {
+        clearInterval(y);
+        document.getElementById("countdown-tournament").innerHTML =
+          "Torneo Terminado";
+      }
+    }, 1000);
+
     HomeService.getFighters().then(function(response) {
       $scope.fighters = response;
     });
